070ea6bd2cca4db7ef0361b777e2056c82383b62
SatioO <vaibhav.satam29991@gmail.com>
2019-02-03 12:36:43 +0530

    feat(hooks): added one more way of composing reducer

--- src/hooks/useReducer/index.js
+++ src/hooks/useReducer/index.js
@@ -1,15 +1,26 @@
 import React, { useReducer } from "react";
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        case "INCREMENT":
-            return state + 1;
-        case "DECREMENT":
-            return state - 1;
-        default:
-            return state;
-    }
-};
+// function reducer(state, action) {
+//     switch (action.type) {
+//         case "INCREMENT":
+//             return state + 1;
+//         case "DECREMENT":
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// }
+
+function reducer(state, action) {
+    const values = {
+        INCREMENT: () => state + 1,
+        DECREMENT: () => state - 1,
+        DEFAULT: () => state
+    };
+
+    return values[action.type] ? values[action.type]() : values["DEFAULT"]();
+}
+
 export default function UseReducer() {
     const [state, dispatch] = useReducer(reducer, 0);
     return (
